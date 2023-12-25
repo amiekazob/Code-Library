840ac7dad0eed41a0d04223f70e0d01d25fad450
Md Samiul Alam Shajal <97245391+amiekazob@users.noreply.github.com>
2023-12-25 21:34:28 +0600

    Update Sieve.cpp

--- Number Theory/Sieve.cpp
+++ Number Theory/Sieve.cpp
@@ -3,19 +3,20 @@ using namespace std;
 
 const int MAX = 1e8 + 100;
 typedef long long ll;
-vector<bool>v(MAX,true);
-Sieve seive(){
+vector<bool>primes(MAX,true);
+void Sieve(){
   for (ll i = 2; i*i <= MAX; i++) {
-    if (v[i]) {
-      for (ll j = i*i; j<= MAX; j+=i)  v[j] = false;  
+    if (primes[i]) {
+      for (ll j = i*i; j<= MAX; j+=i)  primes[j] = false;  
     }
   }
 }
 
 int main() {
   ios_base::sync_with_stdio(0);cin.tie(0);
-  //pre compute all primes upto 10^8
   Sieve();
-  
+
   return 0;
 }
+
+//pre compute all primes upto 10^8
