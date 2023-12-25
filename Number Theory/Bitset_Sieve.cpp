c24d81d7393e603ba136222ce32058c10fabd365
Md Samiul Alam Shajal <97245391+amiekazob@users.noreply.github.com>
2023-12-25 22:21:23 +0600

    Create Bitset_Sieve.cpp

---
+++ Number Theory/Bitset_Sieve.cpp
@@ -0,0 +1,35 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+const int MAX = 1e8 + 100;
+
+typedef long long ll;
+bitset<MAX>primes;
+
+void bitsetSieve() {
+  primes.set();
+  primes[0] = primes[1] = 0;
+  for (ll i = 2; i <= MAX; i+=2) {
+    if (primes[i]) {
+      for ( ll j = i*i ; j <= MAX; j += i) {
+        primes[j] = 0;
+      }
+    }
+  }
+}
+
+int main() {
+  ios_base::sync_with_stdio(0);cin.tie(0);
+  //int n; cin >> n;
+  //bitsetSieve(n);
+  bitsetSieve();
+  /*
+  for ( int i = 1; i <= n; i++) {
+    if (primes[i]) cout << i << " ";
+  }
+  */
+
+  return 0;
+}
+
+//pre compute all primes upto 10^8
