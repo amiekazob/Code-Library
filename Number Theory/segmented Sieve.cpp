c4be02c69adf31bbc6c655b1baebf2f5f5e7d8c0
Md Samiul Alam Shajal <97245391+amiekazob@users.noreply.github.com>
2023-12-29 23:36:24 +0600

    Update segmented Sieve.cpp

--- Number Theory/segmented Sieve.cpp
+++ Number Theory/segmented Sieve.cpp
@@ -1,44 +1,46 @@
-#include<bits/stdc++.h>
+#include <bits/stdc++.h>
 using namespace std;
 
-const int MAX = 1e5+10;
+const int MAX = 1e8 + 10;
 
 typedef long long ll;
-bitset<MAX>marks;
 vector<int> primes;
-void Sieve() {
+
+void sieve() {
+  bitset<MAX>marks;
   marks.set();
-  marks[0] = marks[1] = 0;
-  for (ll i = 2; i <= MAX; i++) {
-    if (marks[i]) {
-      primes.push_back(i);
-      for ( ll j = i*i ; j <= MAX; j += i)  marks[j] = 0;    
+  primes.push_back(2);
+  for (ll i = 3 ; i <= MAX; i+=2) {
+  if (marks[i]) {
+  primes.push_back(i);
+  for (ll j = i*i; j <= MAX; j += i) marks[j] = false;
     }
   }
+
 }
-
 void segSieve (ll l, ll r) {
-  vector<bool>isPrime(r - l + 1,true);
-  ll base = 0;
+  vector<bool>isPrime(r - l + 1, true);
+   ll base = 0;
   for (ll i : primes) {
-  	base = max (i * i, (l + i - 1) / i * i);
-    for (ll j = base; j <= r; j += i) isPrime[j] = false;
+    base = max (i * i, (l + i - 1) / i * i);
+    for (ll j = base; j <= r; j += i)  isPrime[j - l] = false;       
   }
-  for (ll i = 0 ; i <= r - l + 1; i++) {
-  	if (i + l == 1) continue;
-  	if (isPrime[i + l]) cout << i + l<< "\n";
+  for (int i = 0; i < r - l + 1; ++i) {
+    if (i + l == 1) continue;
+    if (isPrime[i]) cout << i + l << "\n";
   }
-
+  
 }
 
 int main() {
-  ios_base::sync_with_stdio(0);cin.tie(0);
-  Sieve();
-  int t; cin >> t;
-  while(t--) {
-  	ll l , r ; cin >> l >> r; 
-    segSieve(l, r);
-    cout<<"\n";
-  }
-  return 0;
+    sieve();
+    int t;
+    cin >> t;
+    ll l, r;
+    while (t--) {
+      cin >> l >> r;
+      segSieve(l, r);
+      cout<<"\n";
+    }
+    return 0;
 }
